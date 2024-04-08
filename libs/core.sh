df742bd392cf588b3520112d68a2a4d01b5c1982
pyong <muandatito@gmail.com>
2024-04-08 08:32:09 +0000

    fix linux ubuntu version

--- libs/core.sh
+++ libs/core.sh
@@ -1,9 +1,9 @@
 _print(){
     local logo="$1"
-     [ ! -e "$logo" ] && exit 1
+     [ ! "$logo" ] && exit 1
     
     while IFS= read -r line; do
-        echo -e "\e[1;33m${line}\e[0m"
+        echo  "\e[1;33m${line}\e[0m"
     done < "$logo"
 
     return 0
