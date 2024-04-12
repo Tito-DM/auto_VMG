b43dd99716c1fc98e3dc87e76f42b6493c63dd63
Tito <muandatito@gmail.com>
2024-04-12 09:55:29 +0100

    Update gitlab_runner_install_ubunut.sh

--- libs/ubuntu-debian/gitlab_runner_install_ubunut.sh
+++ libs/ubuntu-debian/gitlab_runner_install_ubunut.sh
@@ -4,11 +4,11 @@
    [ $(command -v  gitlab-runner  &> /dev/null) ] && return 0
   
 
-  sudo -u root curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+   curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
 
   [ $? != 0 ] && echo -e '\e[1;30m error downloading\e[0m'  return 1
-  sudo -u root apt-get -y install gitlab-runner
+  apt-get -y install gitlab-runner
   _space
-  [ $? = 0 ] && echo -e '\e[1;32m gitlab-runner instalado com sucesso\e[0m' 
+  [ $? = 0 ] && echo  '\e[1;32m gitlab-runner instalado com sucesso\e[0m' 
    _space
  }
